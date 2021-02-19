92d33d3de3807e9e3c15fcd8ec8b70e7402e3ab7
AlecGrey <alecgrey.coding@gmail.com>
2021-02-19 10:37:08 -0800

    forms for user, department, item, and grocery-list, no styling

--- api/routes/admin.py
+++ api/routes/admin.py
@@ -1,7 +1,41 @@
 from flask import Blueprint, render_template
+from api import db
+from api.database import models
 
 admin = Blueprint("admin", __name__, template_folder='../templates')
 
 @admin.route('/', methods=['GET'])
 def index():
     return render_template('admin/index.html')
+
+@admin.route('/users/', methods=['GET'])
+def users():
+    # PRIMARY DATA:
+    return render_template('admin/show.html', table='user', data=None, rel_data=None)
+
+@admin.route('/departments/', methods=['GET'])
+def departments():
+    # PRIMARY DATA:
+    return render_template('admin/show.html', table='department', data=None, rel_data=None)
+
+@admin.route('/items/', methods=['GET'])
+def items():
+    # RELATIONAL DATA:
+    def get_name(dept):
+        return dept.name
+
+    departments = models.Department.query.all()
+    departments.sort(key=get_name)
+    # PRIMARY DATA:
+    return render_template('admin/show.html', table='item', data=None, rel_data=departments)
+
+@admin.route('/lists/', methods=['GET'])
+def lists():
+    # RELATIONAL DATA:
+    def get_lastname(user):
+        return user.lastname
+    
+    users = models.User.query.all()
+    users.sort(key=get_lastname)
+    # PRIMARY DATA:
+    return render_template('admin/show.html', table='list', data=None, rel_data=users)
